816a0b954c85295ef0fd3f78fe187174df16f2b7
Indika Viraj <samangoonerathne@gmail.com>
2025-03-23 19:29:30 +0000

    add little instruction

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# fbAutoPost
-Facebook auto post using selenium
+## Facebook Auto Post using Selenium
+
+### Get Groups List
+
+Use the following code to get a list of Facebook groups:
+
+```javascript
+console.log(
+    JSON.stringify(
+        Array.from(document.getElementsByTagName('div'))
+            .filter(function (item) {
+                if (item.getAttribute('role') == 'listitem' && item.getAttribute('data-visualcompletion') == 'ignore-dynamic') {
+                    return true;
+                }
+            })
+            .map(function (item) {
+                var group = item.innerText.split('\n');
+                return {
+                    name: group[0],
+                    type: group[1],
+                    link: item.getElementsByTagName('a')[0].href.split('?')[0],
+                };
+            })
+    )
+);
+```
---
+++ script.js
@@ -0,0 +1,56 @@
+window.clickShareBtn = function(){
+    var clicked = false;
+    Array.from(document.getElementsByTagName('span')).forEach(function (item){
+        if(item.getAttribute('data-ad-rendering-role')=='share_button'){
+            item.click();
+            clicked = true;
+        }
+    })
+    return clicked;
+}
+window.clickGroupBtn = function(){
+    var clicked = false;
+    Array.from(document.getElementsByTagName('span')).forEach(function (item){
+        if(item.innerHTML=='Group'){
+            item.click();
+            clicked = true;
+        }
+    })
+    return clicked;
+}
+window.focusSearch = function(){
+    var clicked = false;
+    Array.from(document.getElementsByTagName('input')).forEach(function (item){
+        if(item.getAttribute('placeholder')=='Search for groups'){
+            item.focus();
+            clicked = true;
+        }
+    })
+    return clicked;
+}
+window.selectGroup = function(link){
+    var groups = Array.from(document.getElementsByTagName('div')).filter(function (item){
+        if(item.getAttribute('role')=='listitem' && item.getAttribute('data-visualcompletion')=='ignore-dynamic'){
+            return true;
+        }
+    });
+    try{
+        if(groups[0].getElementsByTagName('a')[0].href.split('?')[0]==link){
+            groups[0].getElementsByTagName('div')[0].click();
+            return true;
+        }
+    }
+    catch(e){}
+    return false;
+}
+window.clickPostBtn = function(){
+    var clicked = false;
+    Array.from(document.getElementsByTagName('span')).forEach(function (item){
+        if(item.innerHTML=='Post'){
+            item.click();
+            clicked = true;
+        }
+    })
+    return clicked;
+}
+
