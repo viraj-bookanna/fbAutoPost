58d62bbdc59b639ed784d206014c4b7f8d68ca8c
Indika Viraj <samangoonerathne@gmail.com>
2025-03-23 19:30:17 +0000

    strings used in bot

---
+++ strings.py
@@ -0,0 +1,21 @@
+direct_reply = {
+    '/start': 'hello',
+    '/help': 'use /new to create a new job\ncron generator online: http://indika.rf.gd/crongen.html',
+}
+
+strings = {
+    'ask_post_link': 'Alright, send me the post link to share',
+    'ask_group_list': 'Ok, now send me the groups json file',
+    'invalid_group_list': 'Invalid group list, please send a valid json file',
+    'ask_share_text': 'Great, now send me the share text',
+    'cancelled': 'Cancelled',
+    'cron_help': 'Usage: /cron job_id crontab_expression',
+    'cron_added': 'Cron job added',
+    'cron_invalid': 'Invalid Job ID or crontab expression',
+    'cron_id_invalid': 'Invalid Cron ID',
+    'cron_deleted': 'Cron task deleted',
+    'job_deleted': 'Job deleted',
+    'delete_help': 'Usage: /del JOB|CRON id',
+    'job_invalid': 'Invalid Job ID',
+    'unknown_command': 'Unknown command',
+}
