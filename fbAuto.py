b562189f3101befedd900e72043f9c89116cbceb
Indika Viraj <samangoonerathne@gmail.com>
2025-03-24 20:44:53 +0000

    move to gecko (BMP chars support)

--- fbAuto.py
+++ fbAuto.py
@@ -1,23 +1,31 @@
 import pickle, asyncio
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
 
 class fbAuto:
-    def __init__(self):
+    def __init__(self, headless=True):
         self.logged_in = False
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--window-size=0,0')
-        chrome_options.add_argument('--disable-extensions')
-        chrome_options.add_argument('--disable-gpu')
-        self.chrome_options = chrome_options
+        options = Options()
+        if headless:
+            options.add_argument("--headless")
+        options.add_argument("--start-maximized")
+        prefs = {
+            "dom.webnotifications.enabled": False,
+            "extensions.enabledScopes": 0,
+            "general.useragent.override": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 Firefox/122.0"
+        }
+        profile = webdriver.FirefoxProfile()
+        for key, value in prefs.items():
+            profile.set_preference(key, value)
+        options.profile = profile
+        self.options = options
         self.driver = None
         with open("script.js", "r") as f:
             self.script = f.read()
 
     async def login(self, cookies_file):
-        self.driver = webdriver.Chrome(options=self.chrome_options)
+        self.driver = webdriver.Firefox(options=self.options)
         with open(cookies_file, "rb") as f:
             cookies = pickle.load(f)
         self.driver.get("https://facebook.com")
@@ -32,6 +40,13 @@ class fbAuto:
             await asyncio.sleep(2)
         return False
 
+    async def typeText(self, element, text):
+        for char in text.replace('\r', ''):
+            if char == '\n':
+                element.send_keys(Keys.RETURN)
+                continue
+            element.send_keys(char)
+
     async def sharePost(self, post_link, group, share_text):
         status = False
         self.driver.get(post_link)
@@ -39,10 +54,11 @@ class fbAuto:
         await self.functionWaiter('clickShareBtn')
         await self.functionWaiter('clickGroupBtn')
         await self.functionWaiter('focusSearch')
-        self.driver.switch_to.active_element.send_keys(group['name'])
+        await self.typeText(self, self.driver.switch_to.active_element, group['name'])
+        await asyncio.sleep(2)
         if await self.functionWaiter('selectGroup', f"'{group['link']}'"):
             await asyncio.sleep(2)
-            self.driver.switch_to.active_element.send_keys(share_text)
+            await self.typeText(self.driver.switch_to.active_element, share_text)
             await self.functionWaiter('clickPostBtn')
             await asyncio.sleep(2)
             status = True
